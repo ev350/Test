e7160f588b72bf6b7b5653d13301adc7fed65118
Euan Cowie <euan-cowie@hotmail.co.uk>
2013-10-25 17:07:12 +0100

    Added new class and tested functionality

---
+++ Test/src/com/ev350/test/NewClass.java
@@ -0,0 +1,16 @@
+package com.ev350.test;
+
+public class NewClass {
+	
+	private int a, b;
+
+	public NewClass(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int multiplied() {
+		return a * b;
+	}
+	
+}
--- Test/src/com/ev350/test/Test.java
+++ Test/src/com/ev350/test/Test.java
@@ -9,6 +9,9 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Git is Cool!");
 		System.out.println("\tIsn't it just...");
+		
+		NewClass bum = new NewClass(2, 8);
+		System.out.println(bum.multiplied());
 	}
 
 }
