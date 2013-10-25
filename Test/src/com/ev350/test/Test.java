dc2c8eb2e595766032f12bb6d1e593b698e5796b
Euan Cowie <euan-cowie@hotmail.co.uk>
2013-10-25 16:41:53 +0100

    Added a new print response

--- Test/src/com/ev350/test/Test.java
+++ Test/src/com/ev350/test/Test.java
@@ -8,6 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git is Cool!");
+		System.out.println("\tIsn't it just...");
 	}
 
 }
